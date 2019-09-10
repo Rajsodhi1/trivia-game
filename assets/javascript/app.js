23612b04d441ed878831520324193ce1c2054b82
Raj Sodhi <rajsodhiesq@gmail.com>
2019-09-09 23:16:53 -0700

    committing homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+$("#start").on("click", function() {
+    //console.log("Clicked");
+    $("#subcontainer").remove();
+    for(var i=0; i<questions.length; i++){
+        $("#subcontainer").append("<h2>" + questions[i].question + "</h2>");
+        for( var j=0; j<answers.length; j++){
+            $("#subcontainer").append("<input type='radio' name='question-" + i + "'value='" + questions[i].answers[j] + "'>" +questions[i].answers[j])
+        }
+    }
+})
+
+var questions = [{
+    question:"What is the process for making rose?",
+    answers:["skin contact fermented red grapes", "skin contact fermented white grapes", "no skin contact fermented red grapes", "no skin contact fermented white grapes"],
+    correctAnswer:"no skin contact fermented red grapes"
+    }, {
+    question:"What is the process for making orange wine?",
+    answers:["skin contact fermented red grapes", "skin contact fermented white grapes", "no skin contact fermented red grapes", "no skin contact fermented white grapes"],
+    correctAnswer:"skin contact fermented white grapes"
+    }];
