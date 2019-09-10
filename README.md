efc34e393f51b1d25be50a297d5a0a43a670d60b
Raj Sodhi <rajsodhiesq@gmail.com>
2019-09-09 23:41:53 -0700

    added timer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # trivia-game
+This is supposed to be a simple trivia game about wine. 
+Once you begin this list of questions should populate with a countdown timer at the top
+you must complete the quiz before time runs out
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,7 @@
 $("#start").on("click", function() {
     //console.log("Clicked");
-    $("#subcontainer").remove();
-    for(var i=0; i<questions.length; i++){
-        $("#subcontainer").append("<h2>" + questions[i].question + "</h2>");
-        for( var j=0; j<answers.length; j++){
-            $("#subcontainer").append("<input type='radio' name='question-" + i + "'value='" + questions[i].answers[j] + "'>" +questions[i].answers[j])
-        }
-    }
+    game.start();
+
 })
 
 var questions = [{
@@ -18,3 +13,27 @@ var questions = [{
     answers:["skin contact fermented red grapes", "skin contact fermented white grapes", "no skin contact fermented red grapes", "no skin contact fermented white grapes"],
     correctAnswer:"skin contact fermented white grapes"
     }];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function() {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if(game.counter==0) {
+            game.done();
+        }
+    },
+    start: function() {
+        timer = setInterval(game.countdown, 1000);
+        $("#subcontainer").prepend("<h3>Time Left: <span id = 'counter'>120</span> seconds</h3>");
+        $("#start").remove();
+        for(var i=0; i<questions.length; i++){
+            $("#subcontainer").append("<h2>" + questions[i].question + "</h2>");
+            for( var j=0; j<answers.length; j++){
+                $("#subcontainer").append("<input type='radio' name='question-" + i + "'value='" + questions[i].answers[j] + "'>" +questions[i].answers[j])
+            }
+        }
+    }
+}
